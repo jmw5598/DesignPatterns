ac2decde59d030e6437568c8797879142249a185
Jason White <jmw5598@gmail.com>
2019-07-01 19:17:39 -0400

    created example of builder pattern

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using Builder.Automobiles;
+using Builder.Automobiles.Parts;
+using Builder.Automobiles.Parts.Addons;
+using Builder.Automobiles.Parts.Bodies;
+using Builder.Automobiles.Parts.Engines;
+using Builder.Automobiles.Parts.Frames;
+using Builder.Automobiles.Parts.Wheels;
 
 namespace Builder
 {
@@ -6,7 +13,17 @@ namespace Builder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var automobile = new AutomobileBuilder()
+                .WithFrame(new TruckFrame())
+                .WithBody(new TruckBody())
+                .WithEngine(new V6Engine())
+                .WithWheels(new PremiumWheels())
+                .WithAddon(new Sunroof())
+                .WithAddon(new CDPlayer())
+                .WithAddon(new Bluetooth())
+                .Build();
+            
+            Console.WriteLine(automobile.ToString());
         }
     }
 }
